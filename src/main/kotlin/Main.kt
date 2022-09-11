67b908b0a9e2d984bee400ea30ba837ad37408c6
newbalancer <pnw123456@icloud.com>
2022-09-11 19:00:06 +0900

    feat: ui add

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,38 @@
+import domain.order.OrderService
+import domain.order.OrderServiceImpl
 import domain.product.ProductService
 import domain.product.ProductServiceImpl
+import java.util.Scanner
 
 fun main(args: Array<String>) {
 
+    val sc: Scanner = Scanner(System.`in`)
+
     // product service di
-    val productService : ProductService = ProductServiceImpl()
+    val productService: ProductService = ProductServiceImpl()
 
-    productService.findAll()
+    val orderService: OrderService = OrderServiceImpl()
 
+    val products = productService.findAll()
+
+    println("입력(o[order]: 주문, q[quit]: 종료):")
+
+    val type = sc.next()
+
+    if ("o" == type) {
+
+        orderService.printProduct(products)
+
+        println("상품번호:")
+
+        val num = sc.next()
+
+        println("갯수:")
+
+        val count = sc.next()
+
+
+    } else {
+        sc.close()
+    }
 }
